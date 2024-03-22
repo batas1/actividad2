35552a636ee5ab2b7ed4a09c254952cca4861e99
batas1 <a01663071@tec.mx>
2024-03-21 21:14:21 -0600

    Added random colors to the snake and food

--- snake.py
+++ snake.py
@@ -8,10 +8,17 @@ Exercises
 4. Change the snake to respond to mouse clicks.
 """
 
-from random import randrange
+from random import randrange, choice
 from turtle import *
 
 from freegames import square, vector
+
+# Define a list of five colors (excluding red)
+colors = ['green', 'blue', 'yellow', 'purple', 'orange']
+
+# Initialize food and snake with random colors
+food_color = choice(colors)
+snake_color = choice([color for color in colors if color != food_color])
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
@@ -51,9 +58,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, snake_color)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, food_color)
     update()
     ontimer(move, 100)
 
